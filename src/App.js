1ab263b9db453b98d832f7ae749bf22cb31b67db
igor <embiggenerd@gmail.com>
2020-01-19 15:36:19 -0500

    components wrapped in provider, passing down state and state setter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts'
 
 function App() {
 	const [products] = useState(data);
@@ -16,26 +17,29 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					component={Products}
+					// render={() => (
+					// 	<Products
+					// 		products={products}
+					// 		addItem={addItem}
+					// 	/>
+					// )}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</div>
+		</ProductContext.Provider>
 	);
 }
 
